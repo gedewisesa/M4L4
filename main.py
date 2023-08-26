04587e02efdc6491235370d06f33d189a3bc631e
gedewisesa <132118938+gedewisesa@users.noreply.github.com>
2023-08-26 11:19:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def index():
 def process_form():
     button_python = request.form.get('button_python')
     button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_db = request.form.get('button_db')
 
     email = request.form.get('email')
     text = request.form.get('text')
@@ -25,6 +27,8 @@ def process_form():
     return render_template('index.html', 
                            button_python=button_python,
                            button_discord=button_discord,
+                           button_html=button_html,
+                           button_db=button_db,
                            )
 
 
